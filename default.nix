553470056ae7238e36f264465b37d306f627552d
Sergii Maksymov <ua.maximoff@gmail.com>
2024-03-10 19:57:20 +0100

    initial commit

---
+++ default.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: let
+  inherit (import ./src/packages.nix {inherit pkgs;}) mkPlatformIOPackage;
+  devenvModule = import ./src/platformio.nix;
+in {
+  inherit devenvModule mkPlatformIOPackage;
+
+  gcc-arm = mkPlatformIOPackage "toolchain-gccarmnoneabi" [pkgs.gcc-arm-embedded];
+  openocd = mkPlatformIOPackage "tool-openocd" [pkgs.openocd];
+}
